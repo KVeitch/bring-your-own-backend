119976a2ea1d332b6145beed48b79e0afb245629
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-11-19 07:58:19 -0700

    Add initial data sets to the project

---
+++ apiKey.js
@@ -0,0 +1,6 @@
+// marvel API
+export const publicKey = '19dcdd356a2a43166aed15cef69e19bc'
+export const privateKey = '14cbdbcf36b756b2eeea8b447f3daf73a9627b56'
+export const hash =  'ts=longstring&apikey=19dcdd356a2a43166aed15cef69e19bc&hash=71fcd9f97744257744f065e29f07222a'
+//Your rate limit:3000 calls/day 
+
