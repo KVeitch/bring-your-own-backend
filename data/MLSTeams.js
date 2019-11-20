9273bbe7e4a86e66a653200fac8ba7d477b95a23
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-11-20 16:32:27 -0700

    Add JS object data for seeding DB

---
+++ data/MLSTeams.js
@@ -0,0 +1,142 @@
+const teamsData = [
+  {
+    Club: 'Atlanta United',
+    ClubLogo: 'https://cdn.sofifa.org/teams/2/light/112885.png',
+    City: 'Atlanta',
+    Stadium: 'Mercedes-Benz Stadium'
+  },
+  {
+    Club: 'Chicago Fire',
+    ClubLogo: 'https://cdn.sofifa.org/teams/2/light/693.png',
+    City: 'Chicago',
+    Stadium: 'SeatGeek Stadium'
+  },
+  {
+    Club: 'Colorado Rapids',
+    ClubLogo: 'https://cdn.sofifa.org/teams/2/light/694.png',
+    City: 'Commerce City',
+    Stadium: 'Dick’s Sporting Goods Park'
+  },
+  {
+    Club: 'Columbus Crew SC',
+    ClubLogo: 'https://cdn.sofifa.org/teams/2/light/687.png',
+    City: 'Columbus',
+    Stadium: 'MAPFRE Stadium'
+  },
+  {
+    Club: 'DC United',
+    ClubLogo: 'https://cdn.sofifa.org/teams/2/light/688.png',
+    City: 'Washington DC',
+    Stadium: 'Audi Field'
+  },
+  {
+    Club: 'FC Dallas',
+    ClubLogo: 'https://cdn.sofifa.org/teams/2/light/695.png',
+    City: 'Dallas',
+    Stadium: 'Toyota Stadium'
+  },
+  {
+    Club: 'Houston Dynamo',
+    ClubLogo: 'https://cdn.sofifa.org/teams/2/light/698.png',
+    City: 'Houston',
+    Stadium: 'BBVA Compass Stadium'
+  },
+  {
+    Club: 'LA Galaxy',
+    ClubLogo: 'https://cdn.sofifa.org/teams/2/light/697.png',
+    City: 'Los Angeles',
+    Stadium: 'Dignity Health Sports Park'
+  },
+  {
+    Club: 'Los Angeles FC',
+    ClubLogo: 'https://cdn.sofifa.org/teams/2/light/112996.png',
+    City: 'Los Angeles',
+    Stadium: 'Banc of California Stadium'
+  },
+  {
+    Club: 'Minnesota United FC',
+    ClubLogo: 'https://cdn.sofifa.org/teams/2/light/111138.png',
+    City: 'Minneapolis',
+    Stadium: 'Allianz Field'
+  },
+  {
+    Club: 'Montreal Impact',
+    ClubLogo: 'https://cdn.sofifa.org/teams/2/light/111139.png',
+    City: 'Montreal',
+    Stadium: 'Saputo Stadium'
+  },
+  {
+    Club: 'New England Revolution',
+    ClubLogo: 'https://cdn.sofifa.org/teams/2/light/691.png',
+    City: 'Foxborough',
+    Stadium: 'Gillette Stadium'
+  },
+  {
+    Club: 'New York City FC',
+    ClubLogo: 'https://cdn.sofifa.org/teams/2/light/112828.png',
+    City: 'NewYork City',
+    Stadium: 'Yankee Stadium'
+  },
+  {
+    Club: 'New York Red Bulls',
+    ClubLogo: 'https://cdn.sofifa.org/teams/2/light/689.png',
+    City: 'Harrison',
+    Stadium: 'Red Bull Arena'
+  },
+  {
+    Club: 'Orlando City SC',
+    ClubLogo: 'https://cdn.sofifa.org/teams/2/light/112606.png',
+    City: 'Orlando',
+    Stadium: 'Orlando City Stadium'
+  },
+  {
+    Club: 'Philadelphia Union',
+    ClubLogo: 'https://cdn.sofifa.org/teams/2/light/112134.png',
+    City: 'Chester',
+    Stadium: 'Talen Energy Stadium'
+  },
+  {
+    Club: 'Portland Timbers',
+    ClubLogo: 'https://cdn.sofifa.org/teams/2/light/111140.png',
+    City: 'Portland',
+    Stadium: 'Providence Park'
+  },
+  {
+    Club: 'Real Salt Lake',
+    ClubLogo: 'https://cdn.sofifa.org/teams/2/light/111065.png',
+    City: 'Sandy',
+    Stadium: 'Rio Tinto Stadium'
+  },
+  {
+    Club: 'San Jose Earthquakes',
+    ClubLogo: 'https://cdn.sofifa.org/teams/2/light/111928.png',
+    City: 'San Jose',
+    Stadium: 'Avaya Stadium'
+  },
+  {
+    Club: 'Seattle Sounders FC',
+    ClubLogo: 'https://cdn.sofifa.org/teams/2/light/111144.png',
+    City: 'Seattle',
+    Stadium: 'CenturyLink Field'
+  },
+  {
+    Club: 'Sporting Kansas City',
+    ClubLogo: 'https://cdn.sofifa.org/teams/2/light/696.png',
+    City: 'Kansas City',
+    Stadium: 'Children’s Mercy Park'
+  },
+  {
+    Club: 'Toronto FC',
+    ClubLogo: 'https://cdn.sofifa.org/teams/2/light/111651.png',
+    City: 'Toronto',
+    Stadium: 'BMO Field'
+  },
+  {
+    Club: 'Vancouver Whitecaps FC',
+    ClubLogo: 'https://cdn.sofifa.org/teams/2/light/101112.png',
+    City: 'Vancouver',
+    Stadium: 'BC Place'
+  }
+];
+
+module.exports = teamsData;
