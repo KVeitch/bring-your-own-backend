9dfce10f03021b4aede0a1e7c67bdd414e7474be
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-11-20 16:33:40 -0700

    Add initial DB migration file

--- db/db/migrations/20191119085615_initial.js
+++ db/db/migrations/20191119085615_initial.js
@@ -1,8 +1,33 @@
-
 exports.up = function(knex) {
-  
+  return Promise.all([
+    knex.schema.createTable('teams', table => {
+      table.increments('id').primary();
+      table.string('teamName')
+      table.unique('teamName');
+      table.string('stadium');
+      table.string('logoUrl');
+      table.string('city');
+      table.timestamp(true, true);
+    }),
+
+    knex.schema.createTable('players', table => {
+      table.increments('id').primary();
+      table.string('name');
+      table.string('photoUrl');
+      table.string('nationality');
+      table.string('preferedFoot');
+      table.integer('age').unsigned();
+      table.string('team');
+      table.foreign('team').references('teams.teamName');
+
+      table.timestamp(true, true);
+    })
+  ]);
 };
 
 exports.down = function(knex) {
-  
+  return Promise.all([
+    knex.schema.dropTable('players'),
+    knex.schema.dropTable('teams')
+  ]);
 };
