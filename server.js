63746830aade0d1129e3b04d6f881cdcb876367d
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-11-20 21:25:13 -0700

    Add end points for team, roster by team and players

--- server.js
+++ server.js
@@ -10,29 +10,62 @@ app.use(cors());
 
 app.set('port', process.env.PORT || 3001);
 
-
 app.get('/api/v1/teams', (request, response) => {
-  database('teams').select()
-  .then(teams => {
-    response.status(200).json(teams);
+  database('teams')
+    .select()
+    .then(teams => {
+      response.status(200).json(teams);
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
 
-  })
+app.get('/api/v1/players', (request, response) => {
+  database('players')
+    .select()
+    .then(players => {
+      response.status(200).json(players);
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
 });
 
 app.get('/api/v1/teams/:id', (request, response) => {
   const { id } = request.params;
-  const foundTeam = app.locals.teams.find(team => team.id === parseInt(id));
-  if (!foundTeam) {
-    return response
-      .status(404)
-      .json(`There is no current team with the id of ${id}`);
-  }
-  response.status(200).json(foundTeam);
+  database('teams')
+    .where(function () {
+        this
+          .where('teamname', String(id))
+          .orWhere('id', id)
+      })
+    .then(team => {
+      response.status(200).json(team);
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
 });
+
+app.get('/api/v1/teams/:id/roster', (request, response) => {
+  const { id } = request.params;
+  database('teams')
+  .where({ id: id })
+  .then(team =>{
+    database('players').where({team:team[0].teamname})
+    .then(players => {
+      response.status(200).json(players)
+    })
+  })
+  .catch(error => {
+    response.status(500).json({ error })
+  });
+})
+
 
 app.listen(app.get('port'), () => {
   console.log(
     `${app.locals.title} is running on http://localhost:${app.get('port')}.`
   );
 });
-
