629140461be3ffc32c63e4aeba4fad9954428441
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-11-22 10:22:16 -0700

    Rerange the knexfile

--- knexfile.js
+++ knexfile.js
@@ -19,9 +19,9 @@ module.exports = {
     migrations: {
       directory: './db/migrations'
     },
+    useNullAsDefault: true,
     seeds: {
       directory: './db/seeds/dev'
-    },
-    useNullAsDefault: true
+    }
   }
 };
