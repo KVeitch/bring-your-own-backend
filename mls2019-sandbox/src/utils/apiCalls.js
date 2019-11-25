05aba3b3fa0786eefbaf3dd11e41aff214c5c8c9
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-11-24 17:22:58 -0700

    Add frontend functonality

--- mls2019-sandbox/src/utils/apiCalls.js
+++ mls2019-sandbox/src/utils/apiCalls.js
@@ -1,44 +1,68 @@
 export const getTeams = async () => {
-  let res = await fetch('http://localhost:3001/api/v1/teams')
-  if (!res.ok) {
-    throw Error('Woops! Something went wrong');
-  }
-  let data = await res.json();
+  const res = await fetch('http://localhost:3001/api/v1/teams')
+  const data = await res.json();
   return data
 }
 
 export const getPlayers = async () => {
-  let res = await fetch('http://localhost:3001/api/v1/players')
-  if (!res.ok) {
-    throw Error('Woops! Something went wrong');
-  }
-  let data = await res.json();
+  const res = await fetch('http://localhost:3001/api/v1/players')
+  const data = await res.json();
   return data
 }
 
 export const getPlayer = async (id) => {
-  let res = await fetch(`http://localhost:3001/api/v1/players/${id}`)
-  if (!res.ok) {
-    throw Error('Woops! Something went wrong');
-  }
-  let data = await res.json();
+  const res = await fetch(`http://localhost:3001/api/v1/players/${id}`)
+  const data = await res.json();
   return data
 }
 
 export const getTeam = async (id) => {
-  let res = await fetch(`http://localhost:3001/api/v1/teams/${id}`)
-  if (!res.ok) {
-    throw Error('Woops! Something went wrong');
-  }
-  let data = await res.json();
+  const res = await fetch(`http://localhost:3001/api/v1/teams/${id}`)
+  const data = await res.json();
   return data
 }
 
 export const getRoster = async (id) => {
-  let res = await fetch(`http://localhost:3001/api/v1/teams/${id}/roster`)
-  if (!res.ok) {
-    throw Error('Woops! Something went wrong');
-  }
-  let data = await res.json();
+  const res = await fetch(`http://localhost:3001/api/v1/teams/${id}/roster`)
+  const data = await res.json();
   return data
+}
+
+export const createTeam = async (team) => {
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(team),
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  };
+  const res = await fetch('http://localhost:3001/api/v1/teams/',options)
+  const data = await res.json();
+  return data
+}
+
+export const createPlayer = async (player) => {
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(player),
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  };
+  const res = await fetch('http://localhost:3001/api/v1/players/',options)
+  const data = await res.json();
+  return data
+}
+
+export const deletePlayer = async (id) => {
+  const options = {
+    method: 'DELETE',
+    body: JSON.stringify(id),
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  }
+  const res = await fetch(`http://localhost:3001/api/v1/players/${id}`, options)
+  const data = res.json()
+  return data;
 }
